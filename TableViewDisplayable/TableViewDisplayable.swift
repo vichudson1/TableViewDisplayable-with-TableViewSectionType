57f434ca513c0d080241d16cf72804456d9566d0
Victor Hudson <vichudson1@gmail.com>
2016-03-05 16:29:34 -0500

    Rough Draft of the protocols and extensions with free function implementation.

--- TableViewDisplayable/TableViewDisplayable.swift
+++ TableViewDisplayable/TableViewDisplayable.swift
@@ -6,7 +6,57 @@
 //
 
 import Foundation
-
+/**
+	A simple protocol for structuring data to display and interact with in a tableview. Several free implementation functions are provided in the protocol extension.
+*/
 protocol TableViewDisplayable {
 	var sections: [TableViewSectionType]  { get set }
+	
+	func numberOfSections() -> Int
+	func numberOfRowsInSection(section: Int) -> Int
+	func titleForSection(section: Int) -> String
+	
+	/**
+	Returns the object located at the indexPath
+	
+	- parameter indexPath: IndexPath of the desired Object.
+	
+	- returns: Object located at the IndexPath
+	*/
+	func object(atIndexPath indexPath: NSIndexPath) -> AnyObject
+	func insert(object object:AnyObject, atIndexPath indexPath: NSIndexPath)
+	func removeObject(atIndexPath indexPath: NSIndexPath) -> AnyObject
+	func moveItem(fromIndexPath sourceIP: NSIndexPath, toIndexPath destinationIP: NSIndexPath)
+}
+
+// MARK: - Some Free Method Implementations
+extension TableViewDisplayable {
+	func numberOfSections() -> Int {
+		return sections.count
+	}
+	
+	func numberOfRowsInSection(section: Int) -> Int {
+		return sections[section].rows.count
+	}
+	
+	func titleForSection(section: Int) -> String {
+		return sections[section].header
+	}
+	
+	func objectForIndexPath(indexPath: NSIndexPath) -> AnyObject {
+		return sections[indexPath.section].rows[indexPath.row]
+	}
+	
+	func insert(object object:AnyObject, atIndexPath indexPath: NSIndexPath) {
+		sections[indexPath.section].insert(object: object, atIndex: indexPath.row)
+	}
+
+	mutating func removeObject(atIndexPath indexPath: NSIndexPath) -> AnyObject {
+		return sections[indexPath.section].removeObject(atIndex: indexPath.row)
+	}
+
+	func moveItem(fromIndexPath sourceIP: NSIndexPath, toIndexPath destinationIP: NSIndexPath) {
+		let item = self.removeObject(atIndexPath: sourceIP)
+		self.insert(object: item, atIndexPath: destinationIP)
+	}
 }
--- TableViewDisplayable/TableViewSectionType.swift
+++ TableViewDisplayable/TableViewSectionType.swift
@@ -7,8 +7,23 @@
 //
 
 import Foundation
-
+/**
+	Use this protocol for structs used as the sections of a tableview for TableViewDisplayable. There are methods provided by the extension for adding and removing rows.
+*/
 protocol TableViewSectionType {
-	var sectionHeaderText: String { get set }
+	var header: String { get set }
 	var rows: [AnyObject] { get set }
+	
+	func insert(object object: AnyObject, atIndex index: Int)
+	func removeObject(atIndex index: Int) -> AnyObject
+}
+
+extension TableViewSectionType {
+	mutating func insert(object object: AnyObject, atIndex index: Int) {
+		rows.insert(object, atIndex: index)
+	}
+	
+	mutating func removeObject(atIndex index: Int) -> AnyObject {
+		return rows.removeAtIndex(index)
+	}
 }
