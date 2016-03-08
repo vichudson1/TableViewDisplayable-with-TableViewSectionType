ca771a0f583db6cc6d76a9afe550f75899a57e5c
Victor Hudson <vichudson1@gmail.com>
2016-03-08 10:30:27 -0500

    * Added some more methods to support more features to the API * Added footer properties to the TableviewSectionType protocol.

--- TableViewDisplayable/TableViewDisplayable.swift
+++ TableViewDisplayable/TableViewDisplayable.swift
@@ -10,56 +10,154 @@ import Foundation
 	A simple protocol for structuring data to display and interact with in a tableview. Several free implementation functions are provided in the protocol extension.
 */
 protocol TableViewDisplayable {
-	
+	/// Set `CellItem` to the Type you'll be displaying in your tableview.
 	typealias CellItem
 	
+	/// The Array that represents the sections of your table. These objects should conform to `TableViewSectionType`.
 	var sections: [TableViewSectionType]  { get set }
 	
+	/**
+	The number of sections in your tableview structure.
+	
+	- returns: Int
+	*/
 	func numberOfSections() -> Int
-	func numberOfRowsInSection(section: Int) -> Int
-	func titleForSection(section: Int) -> String
 	
 	/**
-	Returns the object located at the indexPath
+	The number of rows in a section.
 	
-	- parameter indexPath: IndexPath of the desired Object.
+	- parameter section: `Int`: the section you need the row count for.
 	
-	- returns: Object located at the IndexPath
+	- returns: Int
+	*/
+	func numberOfRowsInSection(section section: Int) -> Int
+	
+	/**
+	The title for section header.
+	
+	- parameter section: `Int`: the section you need the header for.
+	
+	- returns: String
+	*/
+	func titleForHeaderForSection(section section: Int) -> String
+	
+	/**
+	The title for the section footer.
+	
+	- parameter section: `Int`: the section you need the footer for.
+	
+	- returns: String
+	*/
+	func titleForFooterForSection(section section: Int) -> String
+	
+	/**
+	Returns the object located at the indexPath.
+	
+	- parameter indexPath: `NSIndexPath`: IndexPath of the desired Object.
+	
+	- returns: Type defined by `CellItem`
 	*/
 	func object(atIndexPath indexPath: NSIndexPath) -> CellItem
-	func insert(object object:AnyObject, atIndexPath indexPath: NSIndexPath)
-	func removeObject(atIndexPath indexPath: NSIndexPath) -> CellItem
-	func moveItem(fromIndexPath sourceIP: NSIndexPath, toIndexPath destinationIP: NSIndexPath)
+	
+	/**
+	An array of all the `CellItem`s in your structure.
+	
+	- returns: Array
+	*/
+	func allItems() -> [CellItem]
+	
+	/**
+	Inserts new items into your data structre.
+	
+	- parameter object:    `CellItem` you want to insert.
+	- parameter indexPath: `NSIndexPath` The location you want the item inserted.
+	*/
+	mutating func insert(object object:CellItem, atIndexPath indexPath: NSIndexPath)
+	
+	/**
+	Remove objects located in your data structure. The removed item is returned to the caller.
+	
+	- parameter indexPath: `NSIndexPath` The location of the object you want to remove.
+	
+	- returns: `CellItem`
+	*/
+	mutating func remove(objectAtIndexPath indexPath: NSIndexPath) -> CellItem
+
+	/**
+	Move `CellItem`s around within your data structure.
+	
+	- parameter sourceIndexPath:      `NSIndexPath` The index path of the `CellItem`s curent location.
+	- parameter destinationIndexPath: `NSIndexPath` The index path of the `CellItem`s new location.
+	*/
+	mutating func moveItem(fromIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath)
+
+	/**
+	Inserts a new `TableViewSectionType` into your data structure.
+	
+	- parameter section: `TableViewSectionType` conforming item representing a section of data in your structure.
+	- parameter index:   `Int` The index you'd like to insert your new section.
+	*/
+	mutating func insert(section section: TableViewSectionType, atIndex index: Int)
+	
+	/**
+	Removes and returns the section an the index passed in.
+	
+	- parameter index:   `Int` The index of the section you want to remove.
+	- returns: `TableViewDescriptionType`
+	*/
+	mutating func remove(sectionAtIndex index: Int) -> TableViewSectionType
 }
 
-// MARK: - Some Free Method Implementations
+
+// MARK: - Method Implementations
 extension TableViewDisplayable {
+	
 	func numberOfSections() -> Int {
 		return sections.count
 	}
 	
-	func numberOfRowsInSection(section: Int) -> Int {
+	func numberOfRowsInSection(section section: Int) -> Int {
 		return sections[section].rows.count
 	}
 	
-	func titleForSection(section: Int) -> String {
+	func titleForHeaderForSection(section section: Int) -> String {
 		return sections[section].header
 	}
 	
-	func objectForIndexPath(indexPath: NSIndexPath) -> CellItem {
+	func titleForFooterForSection(section section: Int) -> String {
+		return sections[section].footer
+	}
+	
+	func object(atIndexPath indexPath: NSIndexPath) -> CellItem {
 		return sections[indexPath.section].rows[indexPath.row] as! CellItem
 	}
 	
-	func insert(object object:CellItem, atIndexPath indexPath: NSIndexPath) {
+	func allItems() -> [CellItem] {
+		var items = [CellItem]()
+		for s in sections {
+			items.appendContentsOf(s.rows.map { $0 as! CellItem })
+		}
+		return items
+	}
+
+	mutating func insert(object object: CellItem, atIndexPath indexPath: NSIndexPath) {
 		sections[indexPath.section].insert(object: object as! AnyObject, atIndex: indexPath.row)
 	}
 
-	mutating func removeObject(atIndexPath indexPath: NSIndexPath) -> CellItem {
+	mutating func remove(objectAtIndexPath indexPath: NSIndexPath) -> CellItem {
 		return sections[indexPath.section].removeObject(atIndex: indexPath.row) as! CellItem
 	}
 
-	func moveItem(fromIndexPath sourceIP: NSIndexPath, toIndexPath destinationIP: NSIndexPath) {
-		let item = self.removeObject(atIndexPath: sourceIP)
-		self.insert(object: item, atIndexPath: destinationIP)
+	mutating func moveItem(fromIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+		let item = self.remove(objectAtIndexPath: sourceIndexPath)
+		self.insert(object: item, atIndexPath: destinationIndexPath)
+	}
+
+	mutating func insert(section section: TableViewSectionType, atIndex index: Int) {
+		sections.insert(section, atIndex: index)
+	}
+
+	mutating func remove(sectionAtIndex index: Int) -> TableViewSectionType {
+		return sections.removeAtIndex(index)		
 	}
 }
--- TableViewDisplayable/TableViewSectionType.swift
+++ TableViewDisplayable/TableViewSectionType.swift
@@ -11,11 +11,41 @@ import Foundation
 	Use this protocol for structs used as the sections of a tableview for TableViewDisplayable. There are methods provided by the extension for adding and removing rows.
 */
 protocol TableViewSectionType {
+	
+	/// `String` The header title for the section.
 	var header: String { get set }
+	
+	/// `String` The footer title for the section.
+	var footer: String { get set }
+	
+	/// `Array` The items contained in your section.
 	var rows: [AnyObject] { get set }
 	
-	func insert(object object: AnyObject, atIndex index: Int)
-	func removeObject(atIndex index: Int) -> AnyObject
+	/**
+	Inserts Items into your section.
+	
+	- parameter object: `AnyObject`: The item you want to insert into your section.
+	- parameter index:  `Int`: The index you want to insert the item to.
+	*/
+	mutating func insert(object object: AnyObject, atIndex index: Int)
+	
+	/**
+	Removes and returns the item located at the passed in index.
+	
+	- parameter index: `Int`: The index of the object to remove.
+	
+	- returns: `AnyObject`
+	*/
+	mutating func removeObject(atIndex index: Int) -> AnyObject
+
+	/**
+	Creates an instance of a Type conforming to `TableViewSectionType`.
+	
+	- parameter header: `String`: The header for the section.
+	- parameter footer: `String`: The footer for the section.
+	- parameter rows:   `[AnyObject]`: An array containing the items for the section.
+	*/
+	init(header:String, footer: String, rows:[AnyObject])
 }
 
 extension TableViewSectionType {
@@ -25,5 +55,9 @@ extension TableViewSectionType {
 	
 	mutating func removeObject(atIndex index: Int) -> AnyObject {
 		return rows.removeAtIndex(index)
+	}
+	
+	init(header:String = "", footer: String = "", rows:[AnyObject] = []) {
+		self.init(header: header, footer: footer, rows: rows)
 	}
 }
