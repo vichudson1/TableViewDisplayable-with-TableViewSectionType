6570759e5721f49b35ce08b469d215a4fc68d179
Victor Hudson <vichudson1@gmail.com>
2016-03-08 10:31:16 -0500

    Switched the sample tableview to use the new protocol based data structure.

--- TableViewDisplayableSample/MasterViewController.swift
+++ TableViewDisplayableSample/MasterViewController.swift
@@ -11,8 +11,8 @@ import UIKit
 class MasterViewController: UITableViewController {
 
 	var detailViewController: DetailViewController? = nil
-	var objects = [AnyObject]()
-
+	
+	var config = TableViewConfig()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -25,6 +25,8 @@ class MasterViewController: UITableViewController {
 		    let controllers = split.viewControllers
 		    self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
 		}
+		
+		buildInitialConfig()
 	}
 
 	override func viewWillAppear(animated: Bool) {
@@ -36,42 +38,39 @@ class MasterViewController: UITableViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	func buildInitialConfig() {
+		let rows = ["Thing 1", "Thing 2"]
+		let section1 = TableViewSection(header: "Things", rows: rows)
+		self.config.insert(section: section1, atIndex: 0)
+		
+		let rows2 = ["Thing 3", "Thing 4", "Thing 5"]
+		let section2 = TableViewSection(header: "More Things", footer: "A bunch more Things", rows: rows2)
+		self.config.insert(section: section2, atIndex: 1)
+	}
 
 	func insertNewObject(sender: AnyObject) {
-		objects.insert(NSDate(), atIndex: 0)
 		let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+		config.insert(object: "Thing \(config.allItems().count + 1)", atIndexPath: indexPath)
 		self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
 	}
 
-	// MARK: - Segues
-
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-		if segue.identifier == "showDetail" {
-		    if let indexPath = self.tableView.indexPathForSelectedRow {
-		        let object = objects[indexPath.row] as! NSDate
-		        let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-		        controller.detailItem = object
-		        controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-		        controller.navigationItem.leftItemsSupplementBackButton = true
-		    }
-		}
-	}
 
 	// MARK: - Table View
 
 	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-		return 1
+		return config.numberOfSections()
 	}
 
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return objects.count
+		return config.numberOfRowsInSection(section: section)
 	}
 
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
 
-		let object = objects[indexPath.row] as! NSDate
-		cell.textLabel!.text = object.description
+		let object = config.object(atIndexPath: indexPath)
+		cell.textLabel!.text = object
 		return cell
 	}
 
@@ -79,14 +78,29 @@ class MasterViewController: UITableViewController {
 		// Return false if you do not want the specified item to be editable.
 		return true
 	}
+	
+	override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		return config.titleForHeaderForSection(section: section)
+	}
+	
+	override func tableView(tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+		return config.titleForFooterForSection(section: section)
+	}
 
 	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
 		if editingStyle == .Delete {
-		    objects.removeAtIndex(indexPath.row)
-		    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-		} else if editingStyle == .Insert {
-		    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-		}
+			config.remove(objectAtIndexPath: indexPath)
+			if config.numberOfRowsInSection(section: indexPath.section) == 0 {
+				config.remove(sectionAtIndex: indexPath.section)
+				tableView.deleteSections(NSIndexSet(index: indexPath.section), withRowAnimation: .Automatic)
+			} else {
+				tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+			}
+		} 
+	}
+	
+	override func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+		config.moveItem(fromIndexPath: sourceIndexPath, toIndexPath: destinationIndexPath)
 	}
 
 
